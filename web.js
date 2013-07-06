c10e646941b9604ec2beced405031eb674a6607a
scheduler <noranovichd@gmail.com>
2013-07-06 00:39:51 -0700

    updated web.js and index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var buffer;
+var string
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+	buffer = fs.readFileSync('./index.html');
+	string = buffer.toString('utf-8');
+	response.send(string);
 });
 
 var port = process.env.PORT || 5000;
